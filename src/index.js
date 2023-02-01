2e72334b309401dc78ecdd9763f4ffd8bc47b626
Artoym1234 <Artoym1234@mail.ru>
2023-02-02 00:02:44 +0300

    step1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
+import './styles.scss';
+import 'bootstrap';
+import * as yup from 'yup';
+import onChange from 'on-change';
+import isEmpty from 'lodash/isEmpty.js';
 
-import  './styles.scss';
-import  'bootstrap';
+const schema = yup.object().shape({
+  website: string().url().nullable(),
+});
 
-console.log("Hello World!");
+const validate = (fields) => {
+  try {
+    schema.validateSync(fields, { abortEarly: false });
+    return {};
+  } catch (e) {
+    return keyBy(e.inner, 'path');
+  }
+};
+
+const validFormUrlInput = () => {
+  const state = {
+    registrationForm: {
+      data: {
+        url: '',
+      },
+      errors: {},
+      submitIsValid: false,
+      valid: true,
+      processState: 'filling',
+      processError: null,
+    },
+  };
+
+  const urlInput = document.querySelector('#url-input');
+  const watchedState = onChange(state, (path, value) => {
+    urlInput.addEventListener('input', (event) => {
+      watchedState.registrationForm.data[event.target.aria-label] = event.target.value;
+      console.log('event.target.value');
+      watchedState.registrationForm.errors = validate(watchedState.registrationForm.data);
+      watchedState.registrationForm.submitIsValid = !!isEmpty(watchedState.registrationForm.valid);
+    });
+  });
+};
